ef26dc68b592da534bf09c9450cb3720840f8f8c
Pares <alfariz2004@gmail.com>
2025-10-16 18:31:13 +0700

    feat: basic routing, handle data(post json), state & shared data

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use axum::{
-    routing::{get, post},
+    routing::{get, post, put},
     http::StatusCode,
     Json, Router,
+    debug_handler
 };
-use axum::extract::Path;
+use axum::extract::{Path, State};
 use serde::{Deserialize, Serialize};
+use std::sync::{Arc, Mutex};
 
 // #[tokio::main]
 // async fn main() {
@@ -68,18 +70,6 @@ use serde::{Deserialize, Serialize};
 // }
 
 
-#[tokio::main]
-async fn main() {
-    let app = Router::new()
-        .route("/", get(|| async { "Halo, dunia!" }))
-        .route("/hello/{name}", get(hello))
-        .route("/goodbye/{name}", get(goodbye))
-        .route("/congrats/{name}", get(congrats));
-
-    println!("Server berjalan di http://localhost:3000");
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
 
 async fn hello(Path(name): Path<String>) -> String {
     format!("Halo, {}!", name)
@@ -91,4 +81,63 @@ async fn goodbye(Path(name): Path<String>) -> String {
 
 async fn congrats(Path(name): Path<String>) -> String {
     format!("Congrats, {}!", name)
+}
+
+#[derive(Deserialize, PartialEq)]
+struct UserInput {
+    username: String,
+}
+
+#[derive(Deserialize, Serialize)]
+struct UserResponse {
+    message: String,
+}
+
+#[debug_handler]
+async fn create_user(Json(v_payload): Json<UserInput> ) -> Result<Json<UserResponse>, StatusCode> {
+    if v_payload.username.trim().is_empty() {
+        return Err(StatusCode::BAD_REQUEST);
+    } else {
+        Ok(Json(UserResponse {
+            message: format!("User {} berhasil dibuat", v_payload.username),
+        }))
+    }
+}
+
+async fn show_count(State(counter): State<Arc<Mutex<u32>>>) -> String {
+    let count = *counter.lock().unwrap();
+    format!("Count sekarang: {}", count)
+}
+
+async fn increase(State(counter): State<Arc<Mutex<u32>>>) -> String {
+    let mut count = counter.lock().unwrap();
+    *count += 1;
+    format!("Count bertambah: {}", *count)
+}
+
+async fn reset_count(State(counter): State<Arc<Mutex<u32>>>) -> String {
+    let mut count = counter.lock().unwrap();
+    *count = 0;
+    format!("Count sekarang: {}", *count)
+}
+
+
+#[tokio::main]
+async fn main() {
+    let counter = Arc::new(Mutex::new(0u32));
+
+    let app = Router::new()
+        .route("/", get(|| async { "Halo, dunia!" }))
+        .route("/hello/{name}", get(hello))
+        .route("/goodbye/{name}", get(goodbye))
+        .route("/congrats/{name}", get(congrats))
+        .route("/users", post(create_user))
+        .route("/count", get(show_count))
+        .route("/count/increase", get(increase))
+        .route("/count/reset", put(reset_count))
+        .with_state(counter);
+
+    println!("Server berjalan di http://localhost:3000");
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
