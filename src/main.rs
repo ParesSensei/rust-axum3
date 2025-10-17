2e658b028167d4a2b892d382e1aaed0cc06f101e
Pares <alfariz2004@gmail.com>
2025-10-17 23:59:28 +0700

    feat: refactor and adding custom error using into_respon trait

--- src/main.rs
+++ src/main.rs
@@ -7,69 +7,33 @@ use axum::{
 use axum::extract::{Path, State};
 use serde::{Deserialize, Serialize};
 use std::sync::{Arc, Mutex};
+use axum::response::{IntoResponse, Response};
+use serde_json::json;
 
-// #[tokio::main]
-// async fn main() {
-//     // initialize tracing
-//     tracing_subscriber::fmt::init();
-//
-//     // build our application with a route
-//     let app = Router::new()
-//         // `GET /` goes to `root`
-//         .route("/", get(root))
-//         // `POST /users` goes to `create_user`
-//         .route("/users", post(create_user))
-//         .route("/users/{id}", get(get_user));
-//
-//     // run our app with hyper, listening globally on port 3000
-//     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-//     axum::serve(listener, app).await.unwrap();
-// }
-//
-// // basic handler that responds with a static string
-// async fn root() -> &'static str {
-//     "Hello, World!"
-// }
-//
-// async fn create_user(
-//     // this argument tells axum to parse the request body
-//     // as JSON into a `CreateUser` type
-//     Json(payload): Json<CreateUser>,
-// ) -> (StatusCode, Json<User>) {
-//     // insert your application logic here
-//     let user = User {
-//         id: 1337,
-//         username: payload.username,
-//     };
-//
-//     // this will be converted into a JSON response
-//     // with a status code of `201 Created`
-//     (StatusCode::CREATED, Json(user))
-// }
-//
-// // the input to our `create_user` handler
-// #[derive(Deserialize)]
-// struct CreateUser {
-//     username: String,
-// }
-//
-// // the output to our `create_user` handler
-// #[derive(Serialize)]
-// struct User {
-//     id: u64,
-//     username: String,
-// }
-//
-//
-// //-----------------Get user --------------//
-// async fn get_user(Path(id): Path<u64>) -> Json<User> {
-//     Json(User{
-//         id,
-//         username: id.to_string(),
-//     })
-// }
+enum AppError {
+    BadRequest(String),
+    NotFound(String),
+    Internal(String),
+}
 
-
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        match self {
+            AppError::BadRequest(msg) => (
+                StatusCode::BAD_REQUEST,
+                Json(json!({"error": msg})),
+                ).into_response(),
+            AppError::NotFound(msg) => (
+                StatusCode::NOT_FOUND,
+                Json(json!({"error": msg})),
+                ).into_response(),
+            AppError::Internal(msg) => (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({ "error": msg })),
+                ).into_response()
+        }
+    }
+}
 
 async fn hello(Path(name): Path<String>) -> String {
     format!("Halo, {}!", name)
@@ -94,14 +58,15 @@ struct UserResponse {
 }
 
 #[debug_handler]
-async fn create_user(Json(v_payload): Json<UserInput> ) -> Result<Json<UserResponse>, StatusCode> {
+async fn create_user(Json(v_payload): Json<UserInput> ) -> Result<Json<UserResponse>, AppError> {
     if v_payload.username.trim().is_empty() {
-        return Err(StatusCode::BAD_REQUEST);
-    } else {
-        Ok(Json(UserResponse {
-            message: format!("User {} berhasil dibuat", v_payload.username),
-        }))
+        return Err(AppError::BAD_REQUEST("Username cannot be empty".into()));
     }
+
+    Ok(Json(UserResponse {
+        message: format!("User {} berhasil dibuat", v_payload.username),
+    }))
+
 }
 
 async fn show_count(State(counter): State<Arc<Mutex<u32>>>) -> String {
